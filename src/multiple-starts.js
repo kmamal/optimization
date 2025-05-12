69d3c3befcd91efe5c16490a18f7dbe30851c273
KM <the.real.laplace@gmail.com>
2025-05-13 01:49:04 +0300

    chore(commit): force

--- src/multiple-starts.js
+++ src/multiple-starts.js
@@ -1,9 +1,8 @@
-const { search } = require('./search')
 
-const multipleStarts = async (numRestarts, algo, problem, options = {}) => {
-	let bestResult = await search(algo, problem, options)
+const multipleStarts = async (numRestarts, search) => {
+	let bestResult = await search()
 	for (let i = 1; i < numRestarts; i++) {
-		const result = await search(algo, problem, options)
+		const result = await search()
 		if (result.value < bestResult.value) { bestResult = result }
 	}
 	return bestResult
